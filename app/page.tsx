8b7a310720dcec9b89d4e8417942543ba4e44e8c
Rajeev <rajeevdash07@gmail.com>
2024-03-08 17:13:22 +0530

    added the LandingPage in the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import LandingPage from './components/LandingPage';
 import OrganizationList from './components/OrganizationList';
 
 export default function Home() {
   return (
     <div>
+      <LandingPage />
       <OrganizationList />
     </div>
   );
