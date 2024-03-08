b605083b97118877e06d2cbe94a4ab0dd74405b3
Rajeev <rajeevdash07@gmail.com>
2024-03-08 16:09:35 +0530

    fixed some of the console errors

--- app/components/OrganizationList.tsx
+++ app/components/OrganizationList.tsx
@@ -10,13 +10,13 @@ const OrganizationList = () => {
       {organizationsData.organizations.map((org) => (
         <div key={org.name} className="group relative rounded-3xl bg-slate-50 p-6 dark:bg-slate-800/80 dark:highlight-white/5 hover:bg-slate-100 dark:hover:bg-slate-700/50">
         <div className='aspect-[672/494] relative rounded-md transform overflow-hidden shadow-[0_2px_8px_rgba(15,23,42,0.08)] bg-slate-200 dark:bg-slate-700'>
-          <img src={org.image_url} alt={org.name} fetchpriority="high" width="672" height="494" decoding="async" data-nimg="1" class="absolute inset-0 w-full h-full" />
+          <img src={org.image_url} alt={org.name} width="672" height="494" decoding="async" data-nimg="1" className="absolute inset-0 w-full h-full" />
           </div>
           <h2 className="text-xl font- mb-2 mt-2">{org.name}</h2>
           <p className="text-gray-400 mb-2">{org.description}</p>
           <p className="text-sm text-gray-500 mb-2">Category: {org.category}</p>
           <p className="text-sm text-gray-500 mb-2">Technologies: {org.technologies.join(', ')}</p>
-          <a href={org.guide_url} target="_blank" rel="noopener noreferrer" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Visit Website</a>
+          <a ref={org.guide_url} target="_blank" rel="noopener noreferrer" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Visit Website</a>
         </div>
       ))}
     </div>
