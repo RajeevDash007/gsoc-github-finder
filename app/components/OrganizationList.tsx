c196c00fe9f044f916c9fd524be2dd8e8a7b26dd
Rajeev <rajeevdash07@gmail.com>
2024-03-10 17:39:27 +0530

    fixing the issue temporarily

--- app/components/OrganizationList.tsx
+++ app/components/OrganizationList.tsx
@@ -113,7 +113,7 @@ const OrganizationList = () => {
             </div>
             <div className="flex justify-center">
               <a
-                href={org.guide_url || org.url || undefined}
+                href={org.url || undefined}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
