5aabd837263ce1cd551d5093aa29885b44eee5e5
Rajeev Lochan Dash <96860040+RajeevDash007@users.noreply.github.com>
2024-03-22 18:32:38 +0530

    Revert "added new navbar"

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Footer from './components/Footer';
 import LandingPage from './components/LandingPage';
-import Navbar from './components/Navbar';
 import OrganizationList from './components/OrganizationList';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <LandingPage />
       <OrganizationList />
       <Footer/>
