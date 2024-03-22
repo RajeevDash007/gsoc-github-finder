fb1ce277b8d0fd78d0db690764e3ac8e312b8042
Rajeev <rajeevdash07@gmail.com>
2024-03-22 18:27:17 +0530

    added new navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from './components/Footer';
 import LandingPage from './components/LandingPage';
+import Navbar from './components/Navbar';
 import OrganizationList from './components/OrganizationList';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <LandingPage />
       <OrganizationList />
       <Footer/>
