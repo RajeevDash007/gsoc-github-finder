aed4e64a5dc3c8e916ee3fbc5d83753e0ba6cdd4
Rajeev <rajeevdash07@gmail.com>
2024-03-25 16:40:16 +0530

    feat: Github Profile Worth Estimator Added

---
+++ app/worthEstimator/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+import { Navbar } from "../components/Navbar";
+import GitHubProfile from "./GitHubProfile";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <GitHubProfile />
+    </div>
+  );
+}
