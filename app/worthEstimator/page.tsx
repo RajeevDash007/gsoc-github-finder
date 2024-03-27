85064d26241c7b33b8b66085f15aa45c6e5b7ee2
Rajeev <rajeevdash07@gmail.com>
2024-03-27 23:38:04 +0530

    changed the UI of alert box

--- app/worthEstimator/page.tsx
+++ app/worthEstimator/page.tsx
@@ -2,16 +2,31 @@
 import { Navbar } from "../components/Navbar";
 import GitHubProfile from "./GitHubProfile";
 import { useUser } from "@clerk/clerk-react";
-
+import { SignInButton } from "@clerk/nextjs";
 export default function Home() {
   const { user } = useUser();
   return (
     <div>
       <Navbar />
       {!user ? (
-        <p className="text-pink-500 flex justify-center items-center mt-40 text-2xl">
-          Please log in to access this page.
-        </p>
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="max-w-md bg-gray-100 p-6 rounded-lg shadow-lg">
+            <div className="flex items-center mb-4">
+              <span className="text-5xl text-yellow-500 mr-2">&#9888;</span>
+              <p className="text-lg font-semibold">
+                Log In to Access this page
+              </p>
+            </div>
+            <SignInButton afterSignInUrl="/worthEstimator">
+              <a
+                className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                href="/sign-in"
+              >
+                Login
+              </a>
+            </SignInButton>
+          </div>
+        </div>
       ) : (
         <div>
           <GitHubProfile />
