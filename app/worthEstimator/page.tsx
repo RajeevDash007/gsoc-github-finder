fc528126023483dc0dd309edced1e9069a1b9be0
Rajeev <rajeevdash07@gmail.com>
2024-03-27 19:58:50 +0530

    Restricted the page for people in guest mode

--- app/worthEstimator/page.tsx
+++ app/worthEstimator/page.tsx
@@ -1,12 +1,22 @@
-import Footer from "../components/Footer";
+"use client";
 import { Navbar } from "../components/Navbar";
 import GitHubProfile from "./GitHubProfile";
+import { useUser } from "@clerk/clerk-react";
 
 export default function Home() {
+  const { user } = useUser();
   return (
     <div>
       <Navbar />
-      <GitHubProfile />
+      {!user ? (
+        <p className="text-pink-500 flex justify-center items-center mt-40 text-2xl">
+          Please log in to access this page.
+        </p>
+      ) : (
+        <div>
+          <GitHubProfile />
+        </div>
+      )}
     </div>
   );
 }
