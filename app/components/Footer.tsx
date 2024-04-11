0f31c982c7e8b197631ea45eef36f9ba1aff3c2e
Rajeev <rajeevdash07@gmail.com>
2024-04-11 11:57:34 +0530

    UI changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,18 +7,18 @@ import "react-social-icons/twitter";
 import "react-social-icons/linkedin";
 const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-slate-800 m-4">
+    <footer className="rounded-lg shadow bg-slate-800 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
-            href="https://flowbite.com/"
+            href="/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <img
               src="https://i.postimg.cc/13JnbbKX/ggf-high-resolution-logo-transparent.png"
               width="80"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
               GSoC Github Finder
             </span>
           </a>
@@ -45,7 +45,7 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
           <a href="https://flowbite.com/" className="hover:underline">
